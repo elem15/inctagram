e8178513b606255323503d75bd7e06aaa73228ce
mmisha2504 <mmisha2504@yandex.ru>
2023-10-06 08:06:44 +0300

    Added translations to Resend Verification Link

--- src/shared/locales/en.ts
+++ src/shared/locales/en.ts
@@ -2,6 +2,8 @@ export const en = {
   test: 'Lorem Ipsum is simply dummy text of the printing',
   resend: {
     title: 'Email verification link expired',
+    message:'Looks like the verification link has expired. Not to worry, we can send the link again',
+    resend_link: 'resend verification link'
   },
   email: {
     title: 'Email sent',
