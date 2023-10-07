f0234208ebd0579f70dd02a59954b0d88a4857d8
Elem <elem15@yandex.ru>
2023-10-07 12:08:11 +0300

    feat: add svg for ru flag

--- src/widgets/langSelect/ui/LangSelect.tsx
+++ src/widgets/langSelect/ui/LangSelect.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent } from 'react'
+'use client'
+import { ChangeEvent, useEffect, useState } from 'react'
 
 import Image from 'next/image'
 import { useRouter } from 'next/router'
@@ -8,9 +9,18 @@ import { FlagRu, FlagUK } from '@/shared/assets'
 export const English = () => {
   return (
     <div className="flex text-base justify-items-center items-baseline">
-      {/* <Image src="/icons/FlagUnitedKingdom.png" alt="" width={20} height={20} /> */}
+      <Image src="/icons/FlagUnitedKingdom.png" alt="" width={20} height={20} />
       {/* <FlagUK /> */}
       <span>English</span>
+    </div>
+  )
+}
+export const Russian = () => {
+  return (
+    <div className="flex text-base justify-items-center items-baseline">
+      {/* <Image src="/icons/FlagUnitedKingdom.png" alt="" width={20} height={20} /> */}
+      <FlagRu />
+      <span>Русский</span>
     </div>
   )
 }
@@ -23,27 +33,30 @@ export const LangSelectWidget = () => {
 
     push({ pathname, query }, asPath, { locale })
   }
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
   return (
     <div className="flex text-base justify-between items-center w-40 h-8 border border-light-500 px-2 py-2">
-      {locale === 'en' ? (
-        <FlagUK />
-      ) : (
-        <Image src="/icons/FlagRu.png" alt="flag Ru" width={20} height={20} />
+      {locale === 'en' ? <FlagUK /> : <FlagRu />}
+      {isClient && (
+        <select
+          onChange={changeLangHandler}
+          defaultValue={locale}
+          className="bg-black text-base border-collapse w-28"
+        >
+          {locales?.map(l => {
+            return (
+              <option value={l} key={l}>
+                {l === 'en' ? <English /> : <Russian />}
+              </option>
+            )
+          })}
+        </select>
       )}
-      <select
-        onChange={changeLangHandler}
-        defaultValue={locale}
-        className="bg-black text-base border-collapse w-28"
-      >
-        {locales?.map(l => {
-          return (
-            <option value={l} key={l}>
-              {l === 'en' ? 'English' : 'Русский'}
-            </option>
-          )
-        })}
-      </select>
     </div>
   )
 }
