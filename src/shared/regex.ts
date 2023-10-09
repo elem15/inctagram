7dd8c6828828b57774771338a0cb357cc787a10f
Elem <elem15@yandex.ru>
2023-10-09 15:01:08 +0300

    fix: INCT-67, regex fix

--- src/shared/regex.ts
+++ src/shared/regex.ts
@@ -1,5 +1,4 @@
-// export const PasswordValidation = /^(?=(?:\S*\d){2})(?=(?:\S*[A-Za-z]){2})(?=\S*[^A-Za-z0-9])\S{8,}/
-export const PasswordValidation = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[\W])(?!=.*[^a-яA-Яё]).*$/
+export const PasswordValidation = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).*$/
 
 export const EmailValidation = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
 
