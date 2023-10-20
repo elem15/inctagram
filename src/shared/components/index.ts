bbf579f8d97421ef51d1a40208c121404940e469
Svyatoslav Nilikovsky <grippiby@gmail.com>
2023-10-20 23:43:06 +0300

    added checkbox and select-box components

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,16 +1,4 @@
-/*export * from './card'
-export * from './select'
-export * from './switcher'*/
 export * from './button'
-/*export * from './slider'
-export * from './dropdown'
-export * from './pagination'
-export * from './table'*/
 export * from './typography'
-/*
-export * from './modal'
-export * from './textfield'
 export * from './checkbox'
-export * from './toast'
-export * from './page'
-*/
+export * from './select'
---
+++ src/shared/components/select/select.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { SelectCustom } from './select'
+
+const meta = {
+  title: 'Components/Select',
+  component: SelectCustom,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof SelectCustom>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DefaultSelect: Story = {
+  args: {
+    options: [
+      { value: 'first', label: 'apple' },
+      { value: 'second', label: 'banana' },
+      { value: 'third', label: 'orange' },
+    ],
+    placeHolder: 'choose options...',
+  },
+}
