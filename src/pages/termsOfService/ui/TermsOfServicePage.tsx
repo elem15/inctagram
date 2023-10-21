5fee106eb9c9d8e85bcc2f547f75ebef6f1841bd
nargiz92 <ada.tag.92@mail.ru>
2023-10-21 16:26:17 +0300

    fix:add internationalization for TermsOfService.tsx

--- src/pages/termsOfService/ui/TermsOfServicePage.tsx
+++ src/pages/termsOfService/ui/TermsOfServicePage.tsx
@@ -1,10 +1,17 @@
+import { useRouter } from 'next/router'
+
 import { getHeaderLayout } from '@/widgets/layouts/header-layout/HeaderLayout'
 import { TermsOfService } from '@/widgets/termsOfService/TermsOfService'
 
 const TermsOfServicePage = () => {
+  const router = useRouter()
+  const handleBackToSignUpClick = () => {
+    router.push('/signup')
+  }
+
   return (
     <div className="flex justify-center items-center bg-dark-700">
-      <TermsOfService onBackToSignUpClick={() => {}} />
+      <TermsOfService onBackToSignUpClick={handleBackToSignUpClick} />
     </div>
   )
 }
