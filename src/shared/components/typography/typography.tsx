20bb801a146642e039d791ca3250dcca5570ee7a
Elem <elem15@yandex.ru>
2023-10-24 23:33:30 +0300

    feat: refactor types options, fix stories argTypes:as

--- src/shared/components/typography/typography.tsx
+++ src/shared/components/typography/typography.tsx
@@ -1,27 +1,35 @@
 import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 
+import clsx from 'clsx'
+
 import s from './typography.module.scss'
+
+export const options = [
+  'large',
+  'h1',
+  'h2',
+  'h3',
+  'regular_text_16',
+  'bold_text_16',
+  'regular_text_14',
+  'medium_text_14',
+  'bold_text_14',
+  'small_text',
+  'semi-bold_small_text',
+  'regular_link',
+  'small_link',
+] as const
+
+export type Options = (typeof options)[number]
 
 export type TypographyProps<T extends ElementType> = {
   as?: T
-  variant?:
-    | 'large'
-    | 'h1'
-    | 'h2'
-    | 'h3'
-    | 'regular_text_16'
-    | 'bold_text_16'
-    | 'regular_text_14'
-    | 'medium_text_14'
-    | 'bold_text_14'
-    | 'small_text'
-    | 'semi-bold_small_text'
-    | 'regular_link'
-    | 'small_link'
+  variant?: Options
 
   children?: ReactNode
   className?: string
 } & ComponentPropsWithoutRef<T>
+
 export const Typography = <T extends ElementType>({
   as,
   variant = 'regular_text_14',
@@ -29,7 +37,7 @@ export const Typography = <T extends ElementType>({
   className,
   ...restProps
 }: TypographyProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof TypographyProps<T>>) => {
-  const classNames = `${s[variant]} ${className}`
+  const classNames = clsx(s[variant], className)
   const Component = as || 'p'
 
   return (
