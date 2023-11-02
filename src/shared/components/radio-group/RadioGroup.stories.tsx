fb7d329d1609927988eef990b008d398f898f422
nargiz92 <ada.tag.92@mail.ru>
2023-11-02 11:16:06 +0300

    refactor: make 2 px for border, correcte RadioGroup.stories.tsx

--- src/shared/components/radio-group/RadioGroup.stories.tsx
+++ src/shared/components/radio-group/RadioGroup.stories.tsx
@@ -33,30 +33,18 @@ export default {
   tags: ['autodocs'],
 } as Meta<typeof RadioGr>
 
-export const Default = {
-  args: {
-    disabled: false,
-    options,
-  },
+export const RadioGroupWithHooks = () => {
+  const [value, setValue] = useState<string>(options[0].value)
+  const handleChangeRdCroup = (value: string) => {
+    setValue(value)
+  }
 
-  render: (args: any) => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [value, setValue] = useState(options[0].value)
-    const handleChangeRdCroup = (value: string) => {
-      setValue(value)
-    }
-
-    return (
-      <div>
-        <RadioGr
-          {...args}
-          onValueChange={handleChangeRdCroup}
-          value={value}
-          options={options}
-          defaultValue={options[0].value}
-        />
-        <div>Selected value: {value}</div>
-      </div>
-    )
-  },
+  return (
+    <RadioGr
+      onValueChange={handleChangeRdCroup}
+      value={value}
+      options={options}
+      defaultValue={options[0].value}
+    />
+  )
 }
