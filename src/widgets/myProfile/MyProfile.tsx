061b447c6eb6de03f314ecfa92077fd4c17650a7
nargiz92 <ada.tag.92@mail.ru>
2023-11-11 14:06:14 +0300

    fix: delete

--- src/widgets/myProfile/MyProfile.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react'
-
-import s from './MyProfile.module.scss'
-
-type Props = {}
-export const MyProfile: FC<Props> = () => {
-  return <div className={s.container}></div>
-}
