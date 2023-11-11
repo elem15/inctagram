04ee9a8ec43581e76ee3f6c70038352020d739ab
nargiz92 <ada.tag.92@mail.ru>
2023-11-11 13:03:14 +0300

    fix: delete unused imports

--- src/widgets/myProfile/MyProfile.tsx
+++ src/widgets/myProfile/MyProfile.tsx
@@ -2,13 +2,7 @@ import { FC } from 'react'
 
 import s from './MyProfile.module.scss'
 
-import { useTranslation } from '@/shared/lib'
-import { ProfilePhotoForGeneralInfo } from '@/widgets/addProfilePhoto/ProfilePhotoForGeneralInfo'
 type Props = {}
 export const MyProfile: FC<Props> = () => {
-  return (
-    <div className={s.container}>
-      <ProfilePhotoForGeneralInfo />
-    </div>
-  )
+  return <div className={s.container}></div>
 }
