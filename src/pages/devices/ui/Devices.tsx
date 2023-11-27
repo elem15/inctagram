a88794a361bf5db07a78132a2c617ae3c6aa3f93
Elem <elem15@yandex.ru>
2023-11-27 23:49:11 +0300

    fix: general information mobile bottom padding

--- src/pages/devices/ui/Devices.tsx
+++ src/pages/devices/ui/Devices.tsx
@@ -3,7 +3,7 @@ import { Devices } from '@/widgets/profileSettings'
 
 const DevicesPage = () => {
   return (
-    <div className="bg-dark-700 pt-10 pl-6 pr-6 md:pr-16 pb-16 md:pb-0">
+    <div className="bg-dark-700 pt-10 pl-6 pr-6 md:pr-16 pb-16 lg:pb-0">
       <Devices />
     </div>
   )
