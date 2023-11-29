f8e23be8fb31ac32ba20dd33175b22769f85be7f
nargiz92 <ada.tag.92@mail.ru>
2023-11-29 11:55:11 +0300

    fix: reduce cod

--- pages/my-profile/following-page/[subscription].tsx
+++ pages/my-profile/following-page/[subscription].tsx
@@ -1,13 +1,7 @@
 import { FollowingAndFollowersPageMob } from '@/widgets/followingAndFollowersPageMob'
 import { getHeaderWithSidebarLayout } from '@/widgets/layouts'
 
-const Type = () => {
-  return (
-    <>
-      <FollowingAndFollowersPageMob />
-    </>
-  )
-}
+const Type = () => <FollowingAndFollowersPageMob />
 
 Type.getLayout = getHeaderWithSidebarLayout
 export default Type
