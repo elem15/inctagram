104aad4d4b85a94200aa0bc0c92288e1ebe5aea3
Elem <elem15@yandex.ru>
2023-11-30 15:49:09 +0300

    fix: remove 3-dot menu if unauth

--- src/widgets/header/ui/HeaderWidget.tsx
+++ src/widgets/header/ui/HeaderWidget.tsx
@@ -54,53 +54,55 @@ export const HeaderWidget: FC = () => {
             </div>
           )}
           <LangSelectWidget />
-          <div className={s.wrappedActionMenu}>
-            <CustomDropdown
-              trigger={
-                <div>
-                  <Typography variant="h1" className={s.content}>
-                    ...
-                  </Typography>
-                </div>
-              }
-              align={'end'}
-              className="flex lg:hidden"
-            >
-              <CustomDropdownItem>
-                <Link
-                  href={'/my-profile/general-information'}
-                  className={clsx(
-                    router.pathname === '/my-profile/general-information' && s.activeLink,
-                    s.content
-                  )}
-                >
-                  <ProfileSettings /> {t.sidebar.settings}
-                </Link>
-              </CustomDropdownItem>
-              <CustomDropdownItem>
-                <Link
-                  href={'/statistics'}
-                  className={clsx(router.pathname === '/statistics' && s.activeLink, s.content)}
-                >
-                  <StatisticsIcon />
-                  {t.sidebar.statistics}
-                </Link>
-              </CustomDropdownItem>
-              <CustomDropdownItem>
-                <Link href={'/favorites'} className={clsx(s.content, s.largeMargin)}>
-                  {router.pathname === '/favorites' ? <BookMarkIcon /> : <FavoritesIcon />}
-                  <span className={router.pathname === '/favorites' ? s.activeLink : ''}>
-                    {t.sidebar.favorites}
-                  </span>
-                </Link>
-              </CustomDropdownItem>
-              <CustomDropdownItem>
-                <div onClick={() => logOut(accessToken as string)} className={s.content}>
-                  <LogOutIcon /> {t.sidebar.log_out}
-                </div>
-              </CustomDropdownItem>
-            </CustomDropdown>
-          </div>
+          {isAuth && (
+            <div className={s.wrappedActionMenu}>
+              <CustomDropdown
+                trigger={
+                  <div>
+                    <Typography variant="h1" className={s.content}>
+                      ...
+                    </Typography>
+                  </div>
+                }
+                align={'end'}
+                className="flex lg:hidden"
+              >
+                <CustomDropdownItem>
+                  <Link
+                    href={'/my-profile/general-information'}
+                    className={clsx(
+                      router.pathname === '/my-profile/general-information' && s.activeLink,
+                      s.content
+                    )}
+                  >
+                    <ProfileSettings /> {t.sidebar.settings}
+                  </Link>
+                </CustomDropdownItem>
+                <CustomDropdownItem>
+                  <Link
+                    href={'/statistics'}
+                    className={clsx(router.pathname === '/statistics' && s.activeLink, s.content)}
+                  >
+                    <StatisticsIcon />
+                    {t.sidebar.statistics}
+                  </Link>
+                </CustomDropdownItem>
+                <CustomDropdownItem>
+                  <Link href={'/favorites'} className={clsx(s.content, s.largeMargin)}>
+                    {router.pathname === '/favorites' ? <BookMarkIcon /> : <FavoritesIcon />}
+                    <span className={router.pathname === '/favorites' ? s.activeLink : ''}>
+                      {t.sidebar.favorites}
+                    </span>
+                  </Link>
+                </CustomDropdownItem>
+                <CustomDropdownItem>
+                  <div onClick={() => logOut(accessToken as string)} className={s.content}>
+                    <LogOutIcon /> {t.sidebar.log_out}
+                  </div>
+                </CustomDropdownItem>
+              </CustomDropdown>
+            </div>
+          )}
         </div>
       </div>
     </header>
