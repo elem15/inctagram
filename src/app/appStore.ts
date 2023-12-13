b9ef7e0ca2d4e9aaaae39f67db92ea1e42b3aef4
nargiz92 <ada.tag.92@mail.ru>
2023-12-13 21:14:10 +0300

    fix:delete for merge

--- src/app/appStore.ts
+++ src/app/appStore.ts
@@ -4,7 +4,6 @@ import { authReducer, authApi } from '../entities/auth'
 
 import { appSlice } from '@/app/services'
 import { countriesApi } from '@/entities/countries/api/countriesApi'
-import { postApi } from '@/entities/post'
 import { profileApi } from '@/entities/profile'
 
 const store = configureStore({
@@ -13,15 +12,13 @@ const store = configureStore({
     [authApi.reducerPath]: authApi.reducer,
     [appSlice.name]: appSlice.reducer,
     [profileApi.reducerPath]: profileApi.reducer,
-    [postApi.reducerPath]: postApi.reducer,
     [countriesApi.reducerPath]: countriesApi.reducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(
       authApi.middleware,
       profileApi.middleware,
-      countriesApi.middleware,
-      postApi.middleware
+      countriesApi.middleware
     ),
 })
 
