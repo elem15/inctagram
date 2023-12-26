a3e3fea521bed5f74ee400e358d12a38fa7d1c3b
Elem <elem15@yandex.ru>
2023-12-26 23:43:15 +0300

    fix: Google redirect url

--- src/shared/constants/ext-urls.ts
+++ src/shared/constants/ext-urls.ts
@@ -6,7 +6,6 @@ export const BASE_WORK_URL = 'https://inctagram.work/api/v1'
 export const BACKEND_URL = `${BASE_URL}/api/v1`
 export const FRONTEND_URL =
   process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : BASE_URL
-
 // 2 different REDIRECT_URL used on deploy end develop
 // const GOOGLE_REDIRECT_URI = FRONTEND_URL + '/auth/oauth-callback-google'
 
@@ -17,5 +16,5 @@ const GOOGLE_REDIRECT_URI =
 
 export const AUTH_URLS = {
   GITHUB: `${BASE_WORK_URL}/auth/github/login`,
-  GOOGLE: `https://accounts.google.com/o/oauth2/v2/auth?scope=email profile&response_type=code&redirect_uri=${GOOGLE_REDIRECT_URI}&client_id=${CLIENT_ID}`,
+  GOOGLE: `https://accounts.google.com/o/oauth2/v2/auth?scope=email profile&response_type=code&redirect_uri=${GOOGLE_REDIRECT_URI}&client_id=${CLIENT_ID}&prompt=consent&access_type=offline&include_granted_scopes=true&enable_granular_consent=true&service=lso&o2v=2&theme=glif&flowName=GeneralOAuthFlow`,
 }
