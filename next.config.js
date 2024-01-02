9b88071faff05551df52f66237452784c6b3a593
Elem <elem15@yandex.ru>
2024-01-02 19:42:47 +0300

    fix: Google Oauth-2

--- next.config.js
+++ next.config.js
@@ -17,8 +17,8 @@ const nextConfig = {
     captcha_site_key: process.env.CAPTCHA_SITE_KEY || '6LeY2y0mAAAAANwI_paCWfoksCgBm1n2z9J0nwNQ',
     google_client_id:
       process.env.GOOGLE_CLIENT_ID ||
-      // '617342613759-f3kbvgm8l310fn40vh6qna2pv8u2uccr.apps.googleusercontent.com',
-      '535513477329-0m3nj9m45g3r0sm8kdh5i8c5jkjs88f0.apps.googleusercontent.com',
+      '617342613759-f3kbvgm8l310fn40vh6qna2pv8u2uccr.apps.googleusercontent.com',
+    // '535513477329-0m3nj9m45g3r0sm8kdh5i8c5jkjs88f0.apps.googleusercontent.com',
   },
   images: {
     remotePatterns: [
