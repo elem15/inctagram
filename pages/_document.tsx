7adde1234b7b1c1865e9ea7cbdac4b59bae9e13c
Elem <elem15@yandex.ru>
2024-01-25 17:29:22 +0300

    feat: new logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <link rel="icon" href="/earth.png" />
+      <link rel="icon" href="/logo-light.png" />
 
       <Head>
         <meta name="darkreader-lock" />
