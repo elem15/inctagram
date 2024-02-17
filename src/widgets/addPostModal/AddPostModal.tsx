9642dc973e5463b9e1a3313a33a3c7b6772d3069
admalla <maasovec@gmail.com>
2024-02-18 01:21:02 +0300

    feat/INCT-232 - fix blinking photo

--- src/widgets/addPostModal/AddPostModal.tsx
+++ src/widgets/addPostModal/AddPostModal.tsx
@@ -168,36 +168,37 @@ export const AddPostModal = ({ openPostModal, closePostModal }: Props) => {
             setImageScr={setImageSrc}
           />
 
-          {imageSrc ? (
-            <AddPostModalData
-              selectPhoto={selectPhotoHandler}
-              closePostModal={closePostModal}
-              setImageScr={setImageSrc}
-            />
-          ) : (
-            <div className={s.modalBox}>
-              {errorText && (
-                <div className={s.errorText}>
-                  <strong>{t.add_profile_photo.error}</strong>
-                  {errorText}
+          {!isOpen &&
+            (imageSrc ? (
+              <AddPostModalData
+                selectPhoto={selectPhotoHandler}
+                closePostModal={closePostModal}
+                setImageScr={setImageSrc}
+              />
+            ) : (
+              <div className={s.modalBox}>
+                {errorText && (
+                  <div className={s.errorText}>
+                    <strong>{t.add_profile_photo.error}</strong>
+                    {errorText}
+                  </div>
+                )}
+                <div className={s.box}>
+                  <DefaultProfileImg style={{ width: '3rem', height: '3rem' }} />
                 </div>
-              )}
-              <div className={s.box}>
-                <DefaultProfileImg style={{ width: '3rem', height: '3rem' }} />
-              </div>
 
-              <Button
-                variant={'primary'}
-                style={{ marginBottom: '24px', width: '168px' }}
-                onClick={selectPhotoHandler}
-              >
-                {t.post.select_button}
-              </Button>
-              <Button variant={'outline'} style={{ width: '170px' }}>
-                {t.post.draft_button}
-              </Button>
-            </div>
-          )}
+                <Button
+                  variant={'primary'}
+                  style={{ marginBottom: '24px', width: '168px' }}
+                  onClick={selectPhotoHandler}
+                >
+                  {t.post.select_button}
+                </Button>
+                <Button variant={'outline'} style={{ width: '170px' }}>
+                  {t.post.draft_button}
+                </Button>
+              </div>
+            ))}
           <input
             accept={'image/jpeg, image/png'}
             onChange={onFileChange}
